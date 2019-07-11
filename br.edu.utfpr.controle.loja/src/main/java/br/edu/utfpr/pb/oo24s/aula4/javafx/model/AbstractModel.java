81581d56aecf75be91ccd38216e030f259ce0960
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2019-07-11 12:54:30 -0300

    Limpado o repositório

--- br.edu.utfpr.controle.loja/src/main/java/br/edu/utfpr/pb/oo24s/aula4/javafx/model/AbstractModel.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.pb.oo24s.aula4.javafx.model;
-
-import java.io.Serializable;
-
-public interface AbstractModel <ID extends  Serializable> 
-            extends Serializable{
-    ID getId();
-}
